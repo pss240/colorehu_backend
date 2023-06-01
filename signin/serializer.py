071fddd76fbabc95ceaa5a56b20600c179898e2b
pss <arp11150@gmail.com>
2023-06-02 03:45:28 +0900

    drf plus

---
+++ signin/serializer.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Signin
+
+class SigninSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Signin
+        fields = ('nickname','email',)
--- signin/views.py
+++ signin/views.py
@@ -1,33 +1,20 @@
 import json
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.views import View
+from rest_framework.decorators import api_view
 
 from .models import Signin
 # Create your views here.
 
-class SigninView(View):
-    def get(self,request):
-        dummy_data = {
-            'nickname':'parkchanyoung',
-            'email':'qjadjehd05@gmail.com'
-        }
-        return JsonResponse(dummy_data)
-    
-    def post(self,request):
-        if request.META['CONTENT_TYPE'] == "application/json":
-            request = json.loads(request.body)
-            user = Signin(nickname = request['nickname'],
-                          email = request['email'])
-        else:
-            user = Signin(nickname = request.POST['nickname'],
-                          email = request.POST['email'])
-        user.save()
-        return HttpResponse(status=200)
-    
-    def put(self,request):
-        return HttpResponse("put요청을 잘 받았다")
-    
-    def delete(self,request):
-        return HttpResponse("delete요청을 잘 받았다")
+@api_view(['POST'])
+def post(request):
+    if request.method == 'POST':
+        request = json.loads(request.body)
+        user = Signin(nickname = request['nickname'],
+                        email = request['email'])
+    else:
+        user = Signin(nickname = request.POST['nickname'],
+                        email = request.POST['email'])
+    user.save()
+    return HttpResponse(status=200)
     
