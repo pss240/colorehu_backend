8eb5557b74cb24cc7f711c03f651baad8ed3d25d
pss <arp11150@gmail.com>
2023-06-02 16:17:26 +0900

    dasd

--- colorehu/urls.py
+++ colorehu/urls.py
@@ -2,9 +2,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-import signin
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signin/',include(signin.urls)),
+    path('',include('signin.urls')),
 ]
