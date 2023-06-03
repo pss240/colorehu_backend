5cebdf16955c3217dd0d6ec70eec47349d9e55b9
pss <arp11150@gmail.com>
2023-06-04 00:22:04 +0900

    asdgh

--- colorset/views.py
+++ colorset/views.py
@@ -24,11 +24,11 @@ def colorSetPost(request):
 @api_view(['POST'])
 def findColorSet(request):
     if request.method == 'POST':
-        json_data = json.loads(request.body.decode('utf-8'))
+        json_data = json.loads(request.body)
         setstr = json_data['str']
         colorSetQuery = ColorSet.objects.all().filter(colorsetstr__contains=setstr)
         colorSetJson = serializers.serialize("json",colorSetQuery)
         
-        return JsonResponse(colorSetJson,status=200)
+        return Response(colorSetJson,status=200)
         
     
