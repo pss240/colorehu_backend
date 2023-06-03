0491ad6b1dbfce717975d96cf3d3718630ca757d
pss <arp11150@gmail.com>
2023-06-03 20:58:52 +0900

    url change

--- colorehu/urls.py
+++ colorehu/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signin',include('signin.urls')),
-    path('colorset',include('colorset.urls')),
+    path('signin/',include('signin.urls')),
+    path('colorset/',include('colorset.urls')),
 ]
