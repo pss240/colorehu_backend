e3f052f09e7a309ac1cae72e38fa23f43297adf3
pss <arp11150@gmail.com>
2023-06-03 22:47:47 +0900

    asd

--- colorset/urls.py
+++ colorset/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'colorset'
 urlpatterns = [
-    path('',views.post,name='colorset'),
+    path('',views.colorSetPost,name='colorset'),
+    path('loadSet',views.findColorSet,name='loadset'),
 ]
