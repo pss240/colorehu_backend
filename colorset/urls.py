89c68343bf8f83932dcf30b273fefb69dac28c9c
pss <arp11150@gmail.com>
2023-06-04 18:27:42 +0900

    asdasd

--- colorset/urls.py
+++ colorset/urls.py
@@ -5,6 +5,6 @@ app_name = 'colorset'
 urlpatterns = [
     path('',views.colorSetPost,name='colorset'),
     path('loadset/<str:colorstr>',views.findRecommendColorSet,name='loadsetrecommendcolorsetlist'),
-    path('loadset/<int:id>',views.findMyColorSet,name='loadsetmycolorsetlist'),
+    path('loadset/<int:pk>',views.findMyColorSet,name='loadsetmycolorsetlist'),
     path('deleteset/<int:pk>',views.deleteColorSet,name='deleteset')
 ]
