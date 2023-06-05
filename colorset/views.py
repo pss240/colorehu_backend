646737c49f0bd293034d296e5d94db3837b5f78c
pss <arp11150@gmail.com>
2023-06-06 00:18:58 +0900

    assd

--- colorset/views.py
+++ colorset/views.py
@@ -39,7 +39,7 @@ def findMyColorSet(request,pk):
     print("inside of findMyColorSet")
     colorSetQuery = ColorSet.objects.all().filter(uid=pk)
     serialized_colorSet = ColorSetSerializer(colorSetQuery,many=True)
-    print(serialized_colorSet)
+    print(serialized_colorSet.data)
     return Response(serialized_colorSet.data,status=200)
 
 @api_view(['GET'])
